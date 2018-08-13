d09160b070708e86b0fed1df885721ca79fb64a4
Sheckerol <checol70@gmail.com>
2018-08-13 12:40:10 -0700

    got server working without sql

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var db = require("./models")
+//var db = require("./models")
 
 app.use(bodyParser.urlencoded({extended: true}));
 
