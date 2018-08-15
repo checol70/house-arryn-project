10ccc2676fca0ab252355cc8a70333170c54a6a1
Sheckerol <checol70@gmail.com>
2018-08-14 20:03:10 -0700

    fixed routes, can now post and get

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,55 +2,76 @@ var path = require("path");
 var express = require("express");
 var db = require("../models");
 
-var recipes = []
-function Recipe(ingredientArr, stepArr, name, originalUser){
+function Recipe(ingredientArr, stepArr, name, originalUser) {
     this.ingredientArr = ingredientArr;
     this.stepArr = stepArr;
     this.name = name;
-    if(originalUser){
+    if (originalUser) {
         this.originalUser = originalUser;
     }
-    recipes.push(this)
 }
 
-function Ingredient(amount, measure, name){
+function Ingredient(amount, measure, name) {
     this.amount = amount;
     this.measure = measure;
     this.name = name;
 }
 
-console.log(JSON.stringify(new Recipe([new Ingredient(1, "item", "Frozen Burrito")], ["microwave burrito", "let it cool", "eat it"], "microwave burritos", 1)))
-module.exports = function(app){
-    app.get("/", function(req, res){
+module.exports = function (app) {
+    app.get("/", function (req, res) {
         // random recipe stuff can go here.
         res.sendFile(path.join(__dirname, "../index.html"));
     })
 
-    app.get("/recipes", function(req,res){
-        //  var hbsObject ={
-        //      recipes: recipes.recipes
-        //  }
-        db.Recipe.all().then(rec=>{
-            console.log("rec= " + rec)
-            var j = JSON.parse(JSON.stringify(rec))
-            console.log("j= ",j)
-            // rec.forEach(element => {
-            //     console.log(JSON.parse(element))
-            // });   
-         res.render(j)
+    app.get("/recipes", function (req, res) {
+        db.Recipe.all().then(rec => {
+            console.log(typeof step)
+            var recips = []
+            for (var i = 0; i < rec.length; i++) {
+                var ing = []
+                var ingred = rec[i].ingredients
+                parsedIng = JSON.parse(ingred)
+                var step = JSON.parse(rec[i].steps)
+
+                for (var j = 0; j < parsedIng.length; j++) {
+                    ing.push(new Ingredient(parsedIng[j].amount, parsedIng[j].measure, parsedIng[j].name));
+                }
+                
+                console.log("ing =", ing)
+                recips.push(new Recipe(ing, step, rec[i].name, rec[i].originalUser));
+
+            }
+            // steps not set right here
+            console.log(recips)
+            var hbsObject = {
+                recipes: recips
+            }
+            res.render("layouts/recipeList", hbsObject)
         })
-        // res.render("recipeList", hbsObject)
     })
-    app.get("/add/recipes", function(req, res){
+
+
+    app.get("/add/recipes", function (req, res) {
         res.sendFile("add.html")
     })
-    app.post("/recipes", function (req, res){
-       +new recipes.Recipe(req.body.ingredients, req.body.steps, req.body.name, req.body.user);
-       console.log(req.body)
-        console.log(recipes.recipes)
-        res.json(req.body);
+
+    // name: DataTypes.STRING,
+    // ingredients: DataTypes.TEXT,
+    // steps: DataTypes.TEXT,
+    // originalUser: DataTypes.INTEGER
+
+
+    app.post("/recipes", function (req, res) {
+        console.log(req.body);
+        db.Recipe.create({
+            name: req.body.name,
+            ingredients: JSON.stringify(req.body.ingredients),
+            steps: JSON.stringify(req.body.steps),
+            originalUser: req.body.originalUser}).then(function(dbRecipe){
+            res.json(dbRecipe)
+        })
     })
-    app.put("/recipes/:id", function (req,res){
+    app.put("/recipes/:id", function (req, res) {
 
     })
 }
