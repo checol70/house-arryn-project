6e0200ce4d548dd4abc4690538ef831edf4caf7b
Carol Martin <carolmartin@cox.net>
2018-08-15 11:15:49 -0700

    changes minor

--- db/sqlseeds.sql
+++ db/sqlseeds.sql
@@ -10,12 +10,3 @@ CREATE TABLE recipes
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Western Cheeseburger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Original Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Origianal Double Burger', false);
-
-SELECT * FROM burgers;
